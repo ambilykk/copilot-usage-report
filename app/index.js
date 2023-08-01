218ea419762746cd9e0c40537b25bffdf8f7d8a9
ambilykk <ambilykk@github.com>
2023-08-01 15:48:30 +0530

    added pagination

--- app/index.js
+++ app/index.js
@@ -19,30 +19,31 @@ let totalSeats = 0;
 
 // Our CSV output fields
 const fields = [
-{
-    label: 'User',
-    value: 'assignee.login'
-},
-{
-    label: 'Last Acivity At',
-    value: 'last_activity_at'
-},
-{
-    label: 'Last Acivity Editor',
-    value: 'last_activity_editor'
-},
-{
-    label: 'Pending Cancellation Date',
-    value: 'pending_cancellation_date'
-}
+    {
+        label: 'User',
+        value: 'assignee.login'
+    },
+    {
+        label: 'Last Acivity At',
+        value: 'last_activity_at'
+    },
+    {
+        label: 'Last Acivity Editor',
+        value: 'last_activity_editor'
+    },
+    {
+        label: 'Pending Cancellation Date',
+        value: 'pending_cancellation_date'
+    }
 ];
 
 // Copilot User Management API call
-async function getUsage(org) {
+async function getUsage(org, pageNo) {
     try {
 
         return await octokit.request('GET /orgs/{org}/copilot/billing/seats', {
             org: org_Name,
+            page: pageNo,
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
@@ -53,30 +54,43 @@ async function getUsage(org) {
     }
 }
 
-// Extract vulnerability alerts with a pagination of 50 alerts per page
+// Extract Copilot usage data with a pagination of 50 records per page
 async function run(org_Name, csv_path) {
 
     let addTitleRow = true;
+    let hasNextPage = false;
+    let pageNo =1;
+    let remainingRecs = 0;
 
     try {
         await makeDir(dirname(csv_path));
+        do {
+            // invoke the graphql query execution
+            await getUsage(org_Name, pageNo).then(usageResult => {
+                let seatsData = usageResult.data.seats;
 
-        // invoke the graphql query execution
-        await getUsage(org_Name).then(usageResult => {           
-            let seatsData = usageResult.data.seats;
-           
-            if (addTitleRow) {
-                totalSeats = usageResult.data.total_seats;
-                console.log('Seat Count ' + totalSeats);
-            }
+                if (addTitleRow) {
+                    totalSeats = usageResult.data.total_seats;
+                    console.log('Seat Count ' + totalSeats);
+                    remainingRecs = totalSeats;
+                }
 
-            // ALERT! - create our updated opts
-            const opts = { fields, "header": addTitleRow };
+                // ALERT! - create our updated opts
+                const opts = { fields, "header": addTitleRow };
 
-            // append to the existing file (or create and append if needed)
-            require("fs").appendFileSync(csv_path, `${parse(seatsData, opts)}\n`);
+                // append to the existing file (or create and append if needed)
+                require("fs").appendFileSync(csv_path, `${parse(seatsData, opts)}\n`);
 
-        });
+                // pagination to get next page data
+                remainingRecs = remainingRecs - seatsData.length;
+                if (remainingRecs > 0) {
+                    pageNo=pageNo+1;
+                    addTitleRow = false;
+                    hasNextPage = true;
+                }
+
+            });
+        } while (hasNextPage);
     } catch (error) {
         core.setFailed(error.message);
     }
